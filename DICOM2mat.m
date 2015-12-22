5f63893f5d205eca83f15e9947127564a196e50f
Jonathan Young <jon.young@utexas.edu>
2015-12-22 17:26:09 -0600

    Initial commit

---
+++ DICOM2mat.m
@@ -0,0 +1,24 @@
+function img = DICOM2mat(dicomDir)
+%
+% Iteratively read in a series of DICOM images into .mat file
+% 
+% Created: 22 December 2015
+% 
+
+cd(dicomDir)
+files = dir('*.dcm');
+Z = length(files);
+
+% Associate slice number with filename
+slice2name = cell(Z,2);
+for k = 1:Z
+    info = dicominfo(files(k).name);
+    slice2name{info.InstanceNumber,1} = info.InstanceNumber;
+    slice2name{info.InstanceNumber,2} = files(k).name;
+end
+
+% Loop through slices in order and save to array
+img = zeros(info.Height, info.Width, Z, 'uint8');
+for k = 1:Z
+    img(:,:,k) = dicomread(slice2name{k,2});
+end
