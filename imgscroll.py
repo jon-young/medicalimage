9db3ca6b294501658dd68798722464fca48ace7e
Jonathan Young <jon.young@utexas.edu>
2016-01-03 21:33:05 -0600

    A single class now handles image display and overlays; function added for plotting

--- imgscroll.py
+++ imgscroll.py
@@ -7,14 +7,17 @@ Created on Mon Nov  2 11:08:10 2015
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
+import SimpleITK as sitk
 
 
 class IndexTracker(object):
-    def __init__(self, ax, X):
+    def __init__(self, ax, X, Xover=None):
         self.ax = ax
         self.X = X
+        self.Xover = Xover
         self.slices, numrows, numcols = X.shape
-        self.ind = 0
+        self.ind = self.slices//2
         
         def format_coord(x, y):
             col = int(x + 0.5)
@@ -24,9 +27,14 @@ class IndexTracker(object):
                 return 'x=%1.2f, y=%1.2f, z=%1.2f' %(x, y, z)
             else:
                 return 'x=%1.2f, y=%1.2f' %(x, y)
-        ax.format_coord = format_coord
         
-        self.im = ax.imshow(self.X[self.ind, :, :], cmap=plt.cm.Greys_r)
+        if self.Xover == None:
+            ax.format_coord = format_coord
+            self.im = ax.imshow(self.X[self.ind,:,:], cmap=plt.cm.Greys_r)
+        else:
+            self.im = ax.imshow(self.X[self.ind,:,:], cmap=plt.cm.Greys_r)
+            self.imOver = ax.imshow(self.Xover[self.ind,:,:], alpha=0.5)
+            
         self.update()
 
     def onscroll(self, event):
@@ -41,32 +49,25 @@ class IndexTracker(object):
         self.im.set_data(self.X[self.ind, :, :])
         self.ax.set_title('slice %s' %self.ind)
         self.im.axes.figure.canvas.draw()
+        if self.Xover != None:
+            self.imOver.set_data(self.Xover[self.ind,:,:])
+            self.imOver.axes.figure.canvas.draw()
 
 
-class IndexTracker2(object):
-    def __init__(self, ax, X, Xover):
-        self.ax = ax
-        self.X = X
-        self.Xover = Xover
-        self.slices = X.shape[0]
-        self.ind = 0
-        
-        self.im = ax.imshow(X[self.ind,:,:], cmap=plt.cm.Greys_r)
-        self.imOver = ax.imshow(Xover[self.ind,:,:], alpha=0.5)
-        self.update()
-        
-    def onscroll(self, event):
-        if event.button == 'up':
-            self.ind = np.clip(self.ind+1, 0, self.slices-1)
-        else:
-            self.ind = np.clip(self.ind-1, 0, self.slices-1)
-
-        self.update()
-    
-    def update(self):
-        self.im.set_data(self.X[self.ind,:,:])
-        self.imOver.set_data(self.Xover[self.ind,:,:])
-        self.ax.set_title('slice %s' %self.ind)
-        self.im.axes.figure.canvas.draw()
-        self.imOver.axes.figure.canvas.draw()
+def show_imgs(imgstack, imgstack2=None):
+    """Uses imgscroll to show stack of images in a new window with 
+    mouse scrolling. If a 2nd image stack is provided, then it will 
+    be overlaid on top of the 1st."""
+    X = sitk.GetArrayFromImage(imgstack)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    if imgstack2 != None:
+        X2 = sitk.GetArrayFromImage(imgstack2)
+        maskVal = scipy.stats.mode(X2.flatten())[0][0]
+        Xmask = np.ma.masked_where(X2 == maskVal, X2)
+        tracker = IndexTracker(ax, X, Xmask)
+    else:
+        tracker = IndexTracker(ax, X)
+    fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
+    plt.show()
 
