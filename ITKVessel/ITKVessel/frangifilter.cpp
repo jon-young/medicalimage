2d6fc64969cd208c27187d7aa5af2af3df257d07
Jonathan Young <jon.young@utexas.edu>
2016-01-20 14:48:49 -0600

    1st working version of Frangi filter

--- ITKVessel/ITKVessel/frangifilter.cpp
+++ ITKVessel/ITKVessel/frangifilter.cpp
@@ -13,6 +13,9 @@
 #include "itkImageSeriesReader.h"
 #include "itkImageSeriesWriter.h"
 #include "itkRegionOfInterestImageFilter.h"
+#include "itkHessianToObjectnessMeasureImageFilter.h"
+#include "itkMultiScaleHessianBasedMeasureImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
 
 
 int main(int argc, const char * argv[])
@@ -28,21 +31,18 @@ int main(int argc, const char * argv[])
     
     const unsigned int Dimension = 3;
     typedef unsigned short PixelType;
-    typedef itk::Image< PixelType, Dimension > InputImageType;
-    typedef itk::Image< PixelType, Dimension > OutputImageType;
-    typedef itk::ImageSeriesReader< InputImageType > ReaderType;
+    typedef itk::Image< PixelType, Dimension > ImageType;
+    typedef itk::ImageSeriesReader< ImageType > ReaderType;
     typedef itk::GDCMImageIO ImageIOType;
     typedef itk::GDCMSeriesFileNames InputNamesGeneratorType;
-    typedef itk::ImageFileWriter< OutputImageType > WriterType;
     const char * outputImage = argv[2];
-    int x_i, x_f, y_i, y_f, z_i, z_f;
     
     ////////////////////////////////////////////////
     // 1) Read the input series
     
     ImageIOType::Pointer gdcmIO = ImageIOType::New();
-    InputNamesGeneratorType::Pointer inputNames =
-      InputNamesGeneratorType::New();
+    InputNamesGeneratorType::Pointer inputNames = 
+		InputNamesGeneratorType::New();
     inputNames->SetInputDirectory( argv[1] );
     
     const ReaderType::FileNamesContainer &filenames =
@@ -63,6 +63,8 @@ int main(int argc, const char * argv[])
     ////////////////////////////////////////////////
     // 2) Extract ROI
     
+	int x_i, x_f, y_i, y_f, z_i, z_f;
+	
     std::cout << "Extracting region-of-interest (ROI)..." << std::endl;
     std::cout << "\nEnter x-coordinate start: ";
     std::cin >> x_i;
@@ -84,31 +86,63 @@ int main(int argc, const char * argv[])
     const itk::IndexValueType startz = static_cast<itk::IndexValueType>(z_i);
     const itk::IndexValueType endz = static_cast<itk::IndexValueType>(z_f);
     
-    InputImageType::IndexType start;
+    ImageType::IndexType start;
     start[0] = startx;
     start[1] = starty;
     start[2] = startz;
     
-    InputImageType::IndexType end;
+    ImageType::IndexType end;
     end[0] = endx;
     end[1] = endy;
     end[2] = endz;
     
-    InputImageType::RegionType region;
+    ImageType::RegionType region;
     region.SetIndex( start );
     region.SetUpperIndex( end );
     
-    typedef itk::RegionOfInterestImageFilter<InputImageType, InputImageType>
-      ROIfilter;
+    typedef itk::RegionOfInterestImageFilter<ImageType, ImageType> ROIfilter;
     ROIfilter::Pointer ROI = ROIfilter::New();
     ROI->SetInput( reader->GetOutput() );
     ROI->SetRegionOfInterest( region );
     
     ////////////////////////////////////////////////
-    // 3) Write output image
+    // 3) Antiga (Frangi-based) filter
+	
+	typedef itk::SymmetricSecondRankTensor< double, Dimension > HessianPixelType;
+	typedef itk::Image< HessianPixelType, Dimension > HessianImageType;
+	typedef itk::HessianToObjectnessMeasureImageFilter<HessianImageType, 
+	ImageType> ObjectnessFilterType;
+	ObjectnessFilterType::Pointer objectnessFilter = ObjectnessFilterType::New();
+	objectnessFilter->SetBrightObject( true );
+	objectnessFilter->SetScaleObjectnessMeasure( false );
+	objectnessFilter->SetAlpha( 0.25 );
+	objectnessFilter->SetBeta( 0.6 );
+	objectnessFilter->SetGamma( 20.0 );
+	objectnessFilter->SetScaleObjectnessMeasure(1);
+	
+	typedef itk::MultiScaleHessianBasedMeasureImageFilter<ImageType, 
+	HessianImageType, ImageType> MultiScaleEnhancementFilterType;
+	MultiScaleEnhancementFilterType::Pointer multiScaleEnhancementFilter = 
+		MultiScaleEnhancementFilterType::New();
+	multiScaleEnhancementFilter->SetInput(ROI->GetOutput());
+	multiScaleEnhancementFilter->SetHessianToMeasureFilter( objectnessFilter );
+	multiScaleEnhancementFilter->SetSigmaStepMethodToEquispaced();
+	multiScaleEnhancementFilter->SetSigmaMinimum( 1.0 );
+	multiScaleEnhancementFilter->SetSigmaMaximum( 4.0 );
+	multiScaleEnhancementFilter->SetNumberOfSigmaSteps( 4 );
+	
+	typedef itk::Image< unsigned char, Dimension > OutputImageType;
+	typedef itk::RescaleIntensityImageFilter< ImageType, OutputImageType > 
+		RescaleFilterType;
+	RescaleFilterType::Pointer rescaleFilter = RescaleFilterType::New();
+	rescaleFilter->SetInput( multiScaleEnhancementFilter->GetOutput() );
+	
+    ////////////////////////////////////////////////
+    // 4) Write output image
     
+	typedef itk::ImageFileWriter< OutputImageType > WriterType;
     WriterType::Pointer writer = WriterType::New();
-    writer->SetInput( ROI->GetOutput() );
+    writer->SetInput( rescaleFilter->GetOutput() );
     writer->SetFileName( outputImage );
     
     try {
